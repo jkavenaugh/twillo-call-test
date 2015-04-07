3a074d7b1cdacf2ba769f5422acb24e391b50e06
jkavenaugh <james.kavenaugh@gmail.com>
2015-04-07 12:24:36 -0400

    Removed Tabs from py

--- multiplecall.py
+++ multiplecall.py
@@ -1,13 +1,14 @@
 from twilio.rest import TwilioRestClient
+
 """
   Configure:
-	account_sid = Your Twillo Account SID
-	auth_token = Your Twillo Auth Token
-	toNumber = A default number to call
-	fromNumber = Your Twillo number
-	xmlPath = Public readable path to your xml. Dropbox will work for this.
-	
-	Change URL to wav file in xml file
+    account_sid = Your Twillo Account SID
+    auth_token = Your Twillo Auth Token
+    toNumber = A default number to call
+    fromNumber = Your Twillo number
+    xmlPath = Public readable path to your xml. Dropbox will work for this.
+    
+    Change URL to wav file in xml file
 """
 account_sid = "12345"
 auth_token = "12345"
@@ -34,16 +35,16 @@ answer = input()
 
 if answer == "y":
 
-	client = TwilioRestClient(account_sid, auth_token)
+    client = TwilioRestClient(account_sid, auth_token)
 
-	for count in range(timesToCall):
-		call = client.calls.create(to= toNumber,
-									from_= fromNumber,
-									url= xmlPath)
-		print("Building call: #", count + 1, end="\r")
-	print("Call building: COMPLETE")
+    for count in range(timesToCall):
+    call = client.calls.create(to= toNumber,
+                               from_= fromNumber,
+                               url= xmlPath)
+    print("Building call: #", count + 1, end="\r")
+    print("Call building: COMPLETE")
 
 else:
-	print("Exiting")
+    print("Exiting")
 
 input("Press enter to close window...")
